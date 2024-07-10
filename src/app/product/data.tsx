46b0e660726a31ec74e48b5b19dc4dd6c7c607a3
BoyCoDon <thenq@dtp-education.com>
2024-07-10 13:18:48 +0700

    ```text refactor: Update DataProduct component to accept query and currentPage as props
    Refactor the DataProduct component to accept the 'query' and 'currentPage' props. This allows for better flexibility and reusability of the component.

--- src/app/product/data.tsx
+++ src/app/product/data.tsx
@@ -5,24 +5,32 @@ import { ScrollArea } from '@radix-ui/react-scroll-area'
 import { ParamsProductProps } from './page';
 import productApiRequest from '@/apiRequests/product';
 import { ResponsePayloadType } from '@/lib/http';
+import { PaginationProduct } from './pagination';
 
-async function DataProduct({ searchParams }: ParamsProductProps) {
-    const currentPage = Number(searchParams?.page) || 1;
+async function DataProduct({
+    query,
+    currentPage,
+}: {
+    query: string;
+    currentPage: number;
+}) {
     const { status, payload } = await productApiRequest.getList2(currentPage) as ResponsePayloadType;
     const data = payload?.data as any;
     // console.log({ data });
     const totalPages = data?.totalPages as number;
     const products = data?.result as ProductResType[];
     return (
-        <ScrollArea className='px-4 py-6 lg:px-8 h-full'>
-            <div className='px-4 py-6 lg:px-8 h-full'>
-                <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 justify-items-center justify-center gap-y-20 gap-x-14 ">
-                    {products?.map((item: ProductResType) => (
-                        <ItemProduct data={item} key={item.IDMon} />
-                    ))}
+        <>
+            <ScrollArea className='px-4 py-6 lg:px-8 h-full'>
+                <div className='px-4 py-6 lg:px-8 h-full'>
+                    <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 justify-items-center justify-center gap-y-20 gap-x-14 ">
+                        {products?.map((item: ProductResType) => (
+                            <ItemProduct data={item} key={item.IDMon} />
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </ScrollArea>
+            </ScrollArea>
+        </>
     )
 }
 
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,6 @@
 import { PaginationProduct } from './pagination';
 import { SidebarProduct } from './SidabarProduct';
 import DataProduct from './data';
-import configEnv from '@/configEnv';
-import productApiRequest from '@/apiRequests/product';
-import { ResponsePayloadType } from '@/lib/http';
-import { ProductResType } from '@/schemaValidations/product.schema';
 
 export type ParamsProductProps = {
     searchParams?: {
@@ -14,20 +10,14 @@ export type ParamsProductProps = {
 }
 export default async function Product({searchParams}: ParamsProductProps) {
     const query = searchParams?.query || '';
-    const page = searchParams?.page || '1';
-    
-    const { status, payload } = await productApiRequest.getList2(Number(page)) as ResponsePayloadType;
-    const data = payload?.data as any;
-    // console.log({ data });
-    const totalPages = data?.totalPages as number;
-    const products = data?.result as ProductResType[];
-
+    const currentPage = searchParams?.page || '1';
+    console.log({query, currentPage});
     return (
         <div className="grid grid-cols-12 h-full">
             <SidebarProduct />
             <div className="flex flex-col col-span-10 overflow-hidden h-full">
-                <DataProduct query={query} currentPage={currentPage} />
-                <PaginationProduct totalPages={totalPages} />
+                <DataProduct query={query} currentPage={Number(currentPage)} />
+                <PaginationProduct totalPages={10} />
             </div>
         </div>
     )
