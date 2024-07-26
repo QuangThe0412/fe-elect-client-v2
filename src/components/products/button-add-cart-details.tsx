68de00e0c4d4298456fc546dc87c7d2e28a720f5
BoyCoDon <thenq@dtp-education.com>
2024-07-26 15:05:37 +0700

    refactor: Improve handling of adding items to cart and loading state

--- src/components/products/button-add-cart-details.tsx
+++ src/components/products/button-add-cart-details.tsx
@@ -1,15 +1,68 @@
 "use client";
+import cartApiRequest from '@/apiRequests/cart';
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { ResponsePayloadType } from '@/lib/http';
+import { CartDetails, CartType } from '@/schemaValidations/cart.schema';
+import useAuthStore, { TypeUsers } from '@/store/auth.store';
+import useCartStore, { TypeCartStore } from '@/store/cart.store';
 
 const ButtonAddCartPageDetails = ({ idProduct }: { idProduct: number }) => {
+    const { user, setIsShowLoginDialog } = useAuthStore((state: TypeUsers) => ({
+        user: state.user,
+        setIsShowLoginDialog: state.setIsShowLoginDialog
+    }))
+    const { cart, setCart, loadingCart, setLoadingCart } = useCartStore((state: TypeCartStore) => ({
+        cart: state.cart,
+        setCart: state.setCart,
+        loadingCart: state.loadingCart,
+        setLoadingCart: state.setLoadingCart
+    }))
 
-    const HandleAddCart = (id: number) => {
-        console.log('Add to cart', id)
+    const IDHoaDon = cart?.IDHoaDon;
+
+    const hadUser = !!(user && Object.keys(user).length);
+
+    const HandleAddCart = async (id: number) => {
+        setLoadingCart(true);
+        if (!hadUser) {
+            setIsShowLoginDialog(true);
+        } else {
+            if (IDHoaDon) {
+                const response = await cartApiRequest.addToCart({ IDHoaDon, IDMon: idProduct, SoLuong: 1 });
+                const { payload, status } = response as ResponsePayloadType;
+                if (status === 200) {
+                    const data = payload?.data as CartDetails[];
+                    const updatedCartDetails = [...(cart?.details || [])];
+
+                    data.forEach((dataItem) => {
+                        const index = updatedCartDetails.findIndex(cartItem => cartItem.IDMon === dataItem.IDMon);
+                        if (index > -1) {
+                            updatedCartDetails[index] = { ...updatedCartDetails[index], ...dataItem };
+                        } else {
+                            updatedCartDetails.push(dataItem);
+                        }
+                    });
+                    setCart({ ...cart, details: updatedCartDetails });
+                }
+            } else {
+                const response = await cartApiRequest.addToCart({ IDMon: idProduct, SoLuong: 1 });
+                const { payload, status } = response as ResponsePayloadType;
+                if (status === 200) {
+                    const newCartGet = await cartApiRequest.getCart();
+                    const { payload, status } = newCartGet as ResponsePayloadType;
+                    if (status === 200) {
+                        const data = payload?.data as CartType;
+                        setCart(data);
+                    }
+                }
+            }
+        }
+        setLoadingCart(false);
     }
 
     return (
-        <Button className='btn btn-primary mt-4' onClick={() => HandleAddCart(idProduct)}>
+        <Button loading={loadingCart} className='btn btn-primary mt-4'
+            onClick={() => HandleAddCart(idProduct)}>
             Thêm vào giỏ hàng
         </Button>
     )
